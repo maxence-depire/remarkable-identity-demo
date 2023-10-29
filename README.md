6a6354f8ca6bab2bd0dc78dc555c3bb1e146e8b0
Max__La__Menace <maxence.depire@gmail.com>
2023-10-29 14:44:33 +0100

    Update scripts.

---
+++ README.md
@@ -0,0 +1,7 @@
+Remarkable Identity Demo
+
+The mistery of (a+b)²
+
+<p>
+    <img src=draw.png width=400px>
+</p>
---
+++ script.py
@@ -0,0 +1,15 @@
+# Basic Value
+a = 9
+b = 3
+
+# Square value
+a_square = pow(a, 2)
+b_square = pow(b, 2)
+
+# Composed square value
+ab_square = a * b
+
+# Total
+total = a_square + b_square + 2 * ab_square
+
+print(total)
